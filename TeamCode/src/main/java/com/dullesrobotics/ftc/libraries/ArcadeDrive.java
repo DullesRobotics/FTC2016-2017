a0e4459cff371e6d3c0ff5b2c15cdbac14604f94
freezone2000 <dev576829@gmail.com>
2017-01-06 19:46:14 -0600

    Added ServoControllerLib to control button servo

--- TeamCode/src/main/java/com/dullesrobotics/ftc/libraries/ArcadeDrive.java
+++ TeamCode/src/main/java/com/dullesrobotics/ftc/libraries/ArcadeDrive.java
@@ -17,14 +17,17 @@ public class ArcadeDrive extends Drivetrain{
     public void driveWithGamepad() {
         Gamepad gamepad = robot.getGamepad1();
         drive(-gamepad.right_stick_x,-gamepad.right_stick_y); //Don't forget the negatives
-
     }
+
+    public void reverseGamepad(){
+        Gamepad gamepad = robot.getGamepad1();
+        drive(gamepad.right_stick_x,gamepad.right_stick_y);
+    }
+
     public void drive(double xPower, double yPower) {
-        if(robot.getFLM() != null) robot.getFLM().setPower(xPower - yPower);
-        if(robot.getFRM() != null) robot.getFRM().setPower(xPower + yPower);
-        if (robot.getFLM() != null) robot.getBLM().setPower(xPower - yPower);
-        if (robot.getBRM() != null) robot.getBRM().setPower(xPower + yPower);
+        if(robot.getFLM() != null) robot.getFLM().setPower(xPower + yPower);
+        if(robot.getFRM() != null) robot.getFRM().setPower(xPower - yPower);
+        if (robot.getBLM() != null) robot.getBLM().setPower(xPower + yPower);
+        if (robot.getBRM() != null) robot.getBRM().setPower(xPower - yPower);
     }
-
-
 }
