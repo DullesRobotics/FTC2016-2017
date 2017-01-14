6c95af63f70fe8eb8cd451cab500ab2baca31f1f
freezone2000 <dev576829@gmail.com>
2017-01-14 16:00:11 -0600

    Autonomous updated. Added Left_Bumper to toggle "quick" mode, which makes the robot move quicker.

--- TeamCode/src/main/java/com/dullesrobotics/ftc/libraries/ArcadeDrive.java
+++ TeamCode/src/main/java/com/dullesrobotics/ftc/libraries/ArcadeDrive.java
@@ -17,17 +17,34 @@ public class ArcadeDrive extends TeleOpDrivetrain{
     @Override
     public void driveWithGamepad() {
         Gamepad gamepad = robot.getGamepad1();
-        drive(-gamepad.right_stick_x,-gamepad.right_stick_y); //Don't forget the negatives
+        drive(-gamepad.right_stick_x, -gamepad.right_stick_y); //Don't forget the negatives
     }
 
     public void reverseGamepad(){
         Gamepad gamepad = robot.getGamepad1();
-        drive(-gamepad.right_stick_x,gamepad.right_stick_y);
+        drive(-gamepad.right_stick_x, gamepad.right_stick_y);
+    }
+
+    public void driveQuicklyWithGamepad(){
+        Gamepad gamepad = robot.getGamepad1();
+        driveQuick(-gamepad.right_stick_x, -gamepad.right_stick_y); //Don't forget the negatives
+    }
+
+    public void reverseQuicklyGamepad(){
+        Gamepad gamepad = robot.getGamepad1();
+        driveQuick(-gamepad.right_stick_x, gamepad.right_stick_y); //Don't forget the negatives
     }
 
     public void drive(double xPower, double yPower) {
         xPower = xPower/3;
         yPower = yPower/1.5;
+        if(robot.getFLM() != null) robot.getFLM().setPower(xPower + yPower);
+        if(robot.getFRM() != null) robot.getFRM().setPower(xPower - yPower);
+        if (robot.getBLM() != null) robot.getBLM().setPower(xPower + yPower);
+        if (robot.getBRM() != null) robot.getBRM().setPower(xPower - yPower);
+    }
+
+    public void driveQuick(double xPower, double yPower) {
         if(robot.getFLM() != null) robot.getFLM().setPower(xPower + yPower);
         if(robot.getFRM() != null) robot.getFRM().setPower(xPower - yPower);
         if (robot.getBLM() != null) robot.getBLM().setPower(xPower + yPower);
