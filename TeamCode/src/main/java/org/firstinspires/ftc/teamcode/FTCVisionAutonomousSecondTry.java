1e7f89187e51740df59aa51598f42c9d7528f431
KDragonM <kennethmitra@gmail.com>
2017-01-13 20:49:01 -0600

    Ftc vision second try

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTCVisionAutonomousSecondTry.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTCVisionAutonomousSecondTry.java
@@ -6,6 +6,7 @@ import com.dullesrobotics.ftc.libraries.RobotWithFlickerShooter;
 import com.dullesrobotics.ftc.libraries.ServoControllerLib;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.lasarobotics.vision.android.Cameras;
@@ -20,14 +21,18 @@ import static com.dullesrobotics.ftc.libraries.commonMethods.delay;
 /**
  * Created by Kenneth on 1/7/2017.
  */
-@Autonomous(name = "UseThis")
+@Autonomous(name = "UseThisBlue")
 public class FTCVisionAutonomousSecondTry extends LinearVisionOpMode {
+    final static double ENCODERTICKSPERREVOLUTION = 1478.4;
+    final static double CIRCUMFERENCEOFWHEELCENTIMETERS = Math.PI*9.6;
+    final static double TICKSPERCENTIMETER = ENCODERTICKSPERREVOLUTION/CIRCUMFERENCEOFWHEELCENTIMETERS;
     private ElapsedTime runtime = new ElapsedTime();
     RobotWithFlickerShooter robot;
     AutonomousDrive autonomousDrive;
     String currentColorOrder = "???, ???";
     int sleepTime = 0;
     ServoControllerLib servoControllerLib;
+    OpticalDistanceSensor ods;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -38,6 +43,7 @@ public class FTCVisionAutonomousSecondTry extends LinearVisionOpMode {
         autonomousDrive = new AutonomousDrive(robot,hardwareMap.opticalDistanceSensor.get("EOPD"));
         servoControllerLib = new ServoControllerLib(hardwareMap.servo.get("btnServo"),180);
         robot.getBLM().setDirection(DcMotorSimple.Direction.REVERSE);
+        ods = hardwareMap.opticalDistanceSensor.get("EOPD");
         //Sets Up Camera
         //initializes camera
         this.setCamera(Cameras.PRIMARY);
@@ -83,13 +89,60 @@ public class FTCVisionAutonomousSecondTry extends LinearVisionOpMode {
         }
 */
         waitForStart(); //Wait for START Button Press on DS
-        delay(sleepTime*1000);
+        //delay(sleepTime*1000);
 
         //START
 
         //Drive forwards until ODS is triggered by white tape
         //autonomousDrive.driveStraightForSetTime(2,.75);
-        autonomousDrive.driveStraightADistanceWithEncoders(30.0);
+        //FWD 10cm
+        autonomousDrive.resetEncoders();
+        autonomousDrive.setRUNTOPOSITION();
+        telemetry.addData("running", "fwd 10 cm");
+        robot.getBLM().setTargetPosition((int)(10.0*TICKSPERCENTIMETER));
+        robot.getBRM().setTargetPosition((int)(10.0*TICKSPERCENTIMETER));
+        robot.getBLM().setPower(.75);
+        robot.getBRM().setPower(0.75);
+        while(opModeIsActive()&&robot.getBLM().getCurrentPosition()<10*TICKSPERCENTIMETER){};
+
+        //Turn 40
+        int ticksToGo = (int) (Math.PI*2.0*14.0*2.54/360.0*40.0*TICKSPERCENTIMETER);
+        autonomousDrive.resetEncoders();
+        autonomousDrive.setRUNTOPOSITION();
+        robot.getBLM().setTargetPosition(ticksToGo);
+        robot.getBLM().setPower(0.3);
+        robot.getBRM().setPower(0.0);
+        while(opModeIsActive()&&robot.getBLM().getCurrentPosition() < ticksToGo&&robot.getBLM().getCurrentPosition() < 100000){}
+        robot.getBLM().setPower(0.0);
+
+
+        //Go straight till EOPD
+        telemetry.addData("Action", "Go straight 30cm");
+        autonomousDrive.resetEncoders();
+        autonomousDrive.setRUNWITHENCODERS();
+        robot.getBLM().setPower(.3);
+        robot.getBRM().setPower(.3);
+        while(opModeIsActive()&&robot.getBLM().getCurrentPosition() < 100000&& ods.getLightDetected() < 0.15){}
+        robot.getBLM().setPower(0.0);
+        robot.getBRM().setPower(0.0);
+
+        //Turn 50
+        ticksToGo = (int) (Math.PI*2.0*14.0*2.54/360.0*50.0*TICKSPERCENTIMETER);
+        autonomousDrive.resetEncoders();
+        autonomousDrive.setRUNTOPOSITION();
+        robot.getBLM().setTargetPosition(ticksToGo);
+        robot.getBLM().setPower(0.3);
+        robot.getBRM().setPower(0.0);
+        while(opModeIsActive()&&robot.getBLM().getCurrentPosition() < ticksToGo&&robot.getBLM().getCurrentPosition() < 100000){}
+        robot.getBLM().setPower(0.0);
+
+
+
+        //Begin Turn
+        autonomousDrive.resetEncoders();
+
+
+        telemetry.addData("Action", "pt turn 30cm");
         autonomousDrive.pointTurn(45.0);
         autonomousDrive.driveStraightTillEOPD(300.0,0.5);//Drive fwd and stop at line
         autonomousDrive.pointTurn(45.0);//face beacon
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test.java
@@ -6,6 +6,7 @@ import com.dullesrobotics.ftc.libraries.ServoControllerLib;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 /**
@@ -16,9 +17,13 @@ public class test extends LinearOpMode {
     private ElapsedTime runtime = new ElapsedTime();
     RobotWithFlickerShooter robot;
     AutonomousDrive autonomousDrive;
+    OpticalDistanceSensor ods;
     String currentColorOrder = "???, ???";
     int sleepTime = 0;
     ServoControllerLib servoControllerLib;
+    final static double ENCODERTICKSPERREVOLUTION = 1478.4;
+    final static double CIRCUMFERENCEOFWHEELCENTIMETERS = Math.PI*9.6;
+    final static double TICKSPERCENTIMETER = ENCODERTICKSPERREVOLUTION/CIRCUMFERENCEOFWHEELCENTIMETERS;
 
 
     @Override
@@ -26,10 +31,28 @@ public class test extends LinearOpMode {
         robot = new RobotWithFlickerShooter(hardwareMap.dcMotor.get("BLM"), hardwareMap.dcMotor.get("BRM"), gamepad1, hardwareMap.dcMotor.get("flickerShooter"));
         autonomousDrive = new AutonomousDrive(robot,hardwareMap.opticalDistanceSensor.get("EOPD"));
         servoControllerLib = new ServoControllerLib(hardwareMap.servo.get("btnServo"),180);
-
+        ods = hardwareMap.opticalDistanceSensor.get("EOPD");
         robot.getBLM().setDirection(DcMotorSimple.Direction.REVERSE);
 
         waitForStart();
-        autonomousDrive.driveStraightForSetTime(3,0.75);
+        autonomousDrive.resetEncoders();
+        autonomousDrive.setRUNTOPOSITION();
+        telemetry.addData("running", "running");
+        robot.getBLM().setTargetPosition((int)(1000));
+        robot.getBRM().setTargetPosition((int)(1000));
+        robot.getBLM().setPower(.75);
+        robot.getBRM().setPower(0.75);
+        while(opModeIsActive()&&robot.getBLM().getCurrentPosition()<1000){};
+
+        autonomousDrive.resetEncoders();
+        autonomousDrive.setRUNWITHENCODERS();
+        robot.getBLM().setPower(.5);
+        robot.getBRM().setPower(.5);
+        while(opModeIsActive()&&robot.getBLM().getCurrentPosition() < 100000&& ods.getLightDetected() < 0.15){
+
+        }
+        robot.getBLM().setPower(0.0);
+        robot.getBRM().setPower(0.0);
+        //autonomousDrive.driveStraightForSetTime(3,0.75);
     }
 }
